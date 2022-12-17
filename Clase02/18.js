da05450f943fc413371f73fa23c9ed7517f3d80a
toleZ <toloyjc@gmail.com>
2022-12-17 12:32:46 -0300

    Diapositiva 18 - Object 'keys', 'values', 'entries'

---
+++ Clase02/18.js
@@ -0,0 +1,11 @@
+const persona = {
+  name: "Mate",
+  lastname: "Naran",
+  age: 3,
+  country: "Arg",
+};
+
+console.log("Object:", persona, "\n");
+console.log("Keys:", Object.keys(persona), "\n");
+console.log("Values:", Object.values(persona), "\n");
+console.log("Entries:", Object.entries(persona), "\n");
