411e088de060b91daab9200489d9bc5aeedb6076
toleZ <toloyjc@gmail.com>
2022-12-17 19:55:15 -0300

    Desafio entregable - 'Clases con ECMAScript y ECMAScript avanzado'

---
+++ Clase02/DesafioEntregable.js
@@ -0,0 +1,30 @@
+class ProductManager {
+  #products;
+
+  constructor() {
+    this.#products = [];
+  }
+
+  addProduct(tile, description, price, thumbnail, code, stock) {
+    const alredyCode = this.#products.some((product) => product.code === code);
+    if (alredyCode)
+      throw error(`There is already a product with the code: ${code}`);
+
+    const id = this.#products.length;
+    const product = { id, tile, description, price, thumbnail, code, stock };
+
+    this.#products.push(product);
+  }
+
+  getProducts() {
+    return this.#products;
+  }
+
+  getProductById(productId) {
+    const product = this.#products.find((product) => product.id === productId);
+
+    return (
+      product || console.error(`No product found with the ID: ${productId}`)
+    );
+  }
+}
